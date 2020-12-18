5cfd87464612da3b079349327f76a8d6466fab2f
CKHarrison <hirohumi1882@gmail.com>
2020-12-18 14:34:20 -0700

    change menu function to give user more options, working on obtaining package status at current time

--- main.py
+++ main.py
@@ -30,15 +30,23 @@ def start_route(car):
     car.move(distance_to_hub, current_location)
 
 
-# get packages delivered by a user specified time if none have been delivered, show current status
-def get_delivered_packages():
-    print('Please enter when you want to see packages delivered.')
+def get_user_time():
+    """gets the time from the user and returns it so other functions can use it"""
+    print('Please enter the time you want to see.')
     try:
         hour = int(input('Please enter hour, in form of 24 hour clock. i.e 9am is 09, 10pm is 22: '))
     except ValueError as e:
         print('Please only enter the hour you wish to see')
         hour = int(input('Please enter hour, in form of 24 hour clock. i.e 9am is 09, 10pm is 22: '))
     minutes = int(input('Please enter minutes from 0 - 60:'))
+    if minutes < 00 or minutes > 60:
+        minutes = int(input('Please enter minutes from 0 - 60'))
+    return hour, minutes
+
+
+def get_delivered_packages():
+    """get packages delivered by a user specified time if none have been delivered, show current status"""
+    hour, minutes = get_user_time()
     # list of all delivered packages if no say none have been delivered and print out status of all packages
     time_list = package_hash.print_delivered_by(hour, minutes)
     if len(time_list) > 0:
@@ -49,6 +57,19 @@ def get_delivered_packages():
         package_hash.print_status()
 
 
+def get_package_status():
+    """function that allows a user to enter a package id number and returns the status of that package"""
+    package_id = int(input("Please enter numerical id of package between 1-40: "))
+    if not isinstance(package_id, int):
+        package_id = int(input('Please enter only valid integer values '))
+    if package_id < 0 or package_id > 40:
+        package_id = int(input('Only enter ranges between 1-40: '))
+
+    hour, time = get_user_time()
+    package = package_hash.lookup(package_id)
+    print(f'Package {package.get_package_id()} status - {package.get_status()}')
+
+
 def correct_address(package_id, address, city, state, zipcode):
     """corrects an incorrect address for any package passed through to a new address"""
     package = package_hash.lookup(package_id)
@@ -57,19 +78,26 @@ def correct_address(package_id, address, city, state, zipcode):
 
 
 if __name__ == '__main__':
+    print('Package status at 7:00AM')
+    package_hash.print_status()
     total_mileage = 0
     truck_one = truck.Truck()
     truck_two = truck.Truck([9, 5])
+
     truck_one_first_load = [14, 15, 16, 34, 26, 22, 24, 19, 20, 21, 1, 7, 29, 2, 33]
-    truck_two_first_load = [31, 32, 25, 6, 36, 13,  39, 27, 35, 3, 8, 30, 5, 37, 38, 40]
     truck_one.load(truck_one_first_load)
+    print('Package status at 8:00AM')
+    package_hash.print_status()
     # get_delivered_packages()
     start_route(truck_one)
     total_mileage += truck_one.get_mileage()
     print('truck_one total mileage: ', truck_one.get_mileage())
     print('truck_one time: ', truck_one.get_time())
 
+    truck_two_first_load = [31, 32, 25, 6, 36, 13, 39, 27, 35, 3, 8, 30, 5, 37, 38, 40]
     truck_two.load(truck_two_first_load)
+    print('Package status at 9:05AM')
+    package_hash.print_status()
     start_route(truck_two)
     print('Truck_two total mileage: ', truck_two.get_mileage())
     print('Truck_two time:', truck_two.get_time())
@@ -88,20 +116,14 @@ if __name__ == '__main__':
 
     # while loop to let user see when packages have been delivered
     while True:
-        response_str = input('Would you like to check when packages have been delivered? Enter Y or N ')
-        response = response_str.lower()
-        if response == 'n':
+        response = input('Would you like to 1) check when packages have been delivered?  2) Inquire about a specific '
+                         'package? Press 3 to quit\n')
+        if response == '3':
             print('Have a nice day, logging you out')
             break
-        else:
+        elif response == '1':
             get_delivered_packages()
-
-
-
-    # test to see what packages have been delivered by 9:30AM and delivered packages overall
-    # package_hash.print_delivered_by(9, 30)
-    # print('*****' * 4)
-    # print('Packages delivered by 10:30:')
-    # package_hash.print_delivered_by(10, 30)
-
-
+        elif response == '2':
+            get_package_status()
+        else:
+            'Please enter a valid response, 1, 2, or 3'
--- package.py
+++ package.py
@@ -126,18 +126,3 @@ def lookup(package_id):
         return None
 
 
-def lookup_alternate(package_id, address=None, city=None, deadline=None, zipcode=None, status=None):
-    package = package_hash.lookup(package_id)
-    if address is not None:
-        package_address = package.get_address()[0] + f'({package.get_address()[-1]})'
-        if package_address:
-            return package_address
-    if city is not None:
-        return package.get_address()[1]
-    if deadline is not None:
-        return package.get_delivery_deadline()
-    if zipcode is not None:
-        return package.get_address()[-1]
-    if status is not None:
-        return package.get_status()
-
